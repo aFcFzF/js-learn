78b9c687a499961b14272886b6eb158efef189dd
markjia <markjia@tencent.com>
2023-06-29 18:00:36 +0800

    git rm ignore

--- packages/run-expr/lib/const/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file index.ts
- * @author afcfzf(9301462@qq.com)
- */
-import type { ModeType, ScopeValueNotExist } from '../types';
-export declare const DEFAULT_ECMA_VERSION = 2021;
-export declare const DEFAULT_INTERPRETER_MODE: ModeType;
-export declare const SCOPE_VALUE_NOT_EXIST: ScopeValueNotExist;
-export declare const EVAL_FUNCTION_IDENTIFIER = "EVAL_FUNCTION_IDENTIFIER";
--- packages/run-expr/lib/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @file index.ts
- * @author afcfzf(9301462@qq.com)
- */
-export type ModeType = 'expr' | 'full' | 'custom';
-export type ScopeValueNotExist = 'SCOPE_VALUE_NOT_EXIST';
